f0327d9a458c5378a9172c389f381be450498332
Amr Elsekilly <amr.e.dev@gmail.com>
2017-02-20 14:42:27 +0200

    learned about variables

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php echo 'hello world!'; ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Learning PHP</title>
+  </head>
+  <body>
+    <?php $name = "Amr Elsekilly" ?>
+    <p>
+      <?php echo "Hello $name!"; ?>
+    </p>
+
+    <h5>
+      <?php echo "How are you $name?"; ?>
+    </h5>
+  </body>
+</html>
